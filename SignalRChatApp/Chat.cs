fd3b57b20d0bee62977a416dd2fa7b41559187c9
TheLurkingDev <thelurkingdev@gmail.com>
2018-08-01 21:32:59 -0400

    Build the server, add SignalR service to the container, and configure the HTTP request pipeline.

---
+++ SignalRChatApp/Chat.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRChatApp
+{
+    public class Chat : Hub
+    {
+        public Task Send(string sender, string message)
+        {
+            return Clients.All.SendAsync("UpdateChat", sender, message);
+        }
+
+        public Task ArchiveChat(string archivedBy, string path, string messages)
+        {
+            string fileName = "ChatArchive" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm") + ".txt";
+            System.IO.File.WriteAllText(path + "\\" + fileName, messages);
+            return Clients.All.SendAsync("Archived", "Chat archived by " + archivedBy);
+        }
+    }
+}
